9ba8e4c7803e064388108ed67fd62ef05b75ad3f
Luis-Pablo <pabloarias024@gmail.com>
2023-06-08 03:55:45 +0000

    funcion gardar y getter y setter listos

--- src/models/Note.php
+++ src/models/Note.php
@@ -13,5 +13,34 @@ class Note extends Database{
 
         $this->uuid = uniqid();
     }
+    public function save(){
+        $query = $this->connect()->prepare("INSERT INTO notes (uuid, title, content, updated) VALUES(:uuid, :title, :content, NOW()");
+        $query->execute(['title' => $this->title, 'uuid' => $this->uuid, 'content' => $this->content]);
+
+    }
+
+    public function getUUID(){
+        return $this->uuid;
+    }
+
+    public function setUUID($value){
+        $this->uuid = $value;
+    }
+
+    public function getTitle(){
+        return $this->title;
+    }
+
+    public function setTitle($value){
+        $this->title = $value;
+    }
+
+    public function getContent(){
+        return $this->content;
+    }
+
+    public function setContent($value){
+        $this->content = $value;
+    }
 }
 
